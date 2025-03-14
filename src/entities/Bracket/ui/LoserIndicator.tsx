6c852288f14339dcc3c619566348cb7dd6c554c2
erikleisinger <erikleisinger@gmail.com>
2025-03-14 12:21:02 -0400

    prevent clicking team name / loser indicator on game from scrolling to that game

--- src/entities/Bracket/ui/LoserIndicator.tsx
+++ src/entities/Bracket/ui/LoserIndicator.tsx
@@ -1,30 +1,18 @@
 import { useContext } from "react";
 import { BracketContext } from "@/shared/Bracket/BracketContext";
-import { BracketEditingContext } from "@/shared/EditableBracket/BracketEditingContext";
 
 export default function LoserIndicator({
   loserTo,
 }: {
   loserTo: string | null;
 }) {
-  const { readableIdIndex, scrollToGame } = useContext(BracketContext);
-  const { lookingForLoserConnection } = useContext(BracketEditingContext);
+  const { readableIdIndex } = useContext(BracketContext);
 
   const baseButtonStyles =
     "text-destructive  rounded-sm  bracket-game__loser-indicator ";
 
-  function onClick(e) {
-    if (!loserTo) return;
-    if (lookingForLoserConnection) return;
-    e.stopPropagation();
-    scrollToGame(loserTo, {
-      block: "center",
-      inline: "center",
-    });
-  }
-
   return (
-    <div className={baseButtonStyles} onClick={onClick}>
+    <div className={baseButtonStyles}>
       Loser {loserTo ? readableIdIndex[loserTo] : "out"}
     </div>
   );
